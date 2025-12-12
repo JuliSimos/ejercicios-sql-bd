070415e0e16e60b7e1506036db6d994940d8ab2c
Julieta Simos <juliet.simos@gmail.com>
2025-12-12 01:21:06 -0300

    se sube script para la creacion de la tabla del ej 1 tp3

---
+++ 02-modelo-fisico (tablas)/Tp3_Ej_1.1_create.sql
@@ -0,0 +1,106 @@
+-- Created by Redgate Data Modeler (https://datamodeler.redgate-platform.com)
+-- Last modification date: 2025-12-12 04:08:19.46
+
+-- tables
+-- Table: AUDIO
+CREATE TABLE AUDIO (
+    id_coleccion int  NOT NULL,
+    id_objeto int  NOT NULL,
+    formato varchar(60)  NOT NULL,
+    duracion int  NOT NULL,
+    CONSTRAINT AUDIO_pk PRIMARY KEY (id_coleccion,id_objeto)
+);
+
+-- Table: COLECCION
+CREATE TABLE COLECCION (
+    id_coleccion serial  NOT NULL,
+    titulo_coneccion varchar(60)  NOT NULL,
+    descripcion varchar(160)  NOT NULL,
+    CONSTRAINT COLECCION_pk PRIMARY KEY (id_coleccion)
+);
+
+-- Table: DOCUMENTO
+CREATE TABLE DOCUMENTO (
+    id_coleccion int  NOT NULL,
+    id_objeto int  NOT NULL,
+    tipo_publicacion varchar(30)  NOT NULL,
+    modos_color int  NOT NULL,
+    resolucion_captura varchar(60)  NOT NULL,
+    CONSTRAINT DOCUMENTO_pk PRIMARY KEY (id_coleccion,id_objeto)
+);
+
+-- Table: OBJETO
+CREATE TABLE OBJETO (
+    id_coleccion int  NOT NULL,
+    id_objeto serial  NOT NULL,
+    id_repo int  NOT NULL,
+    titulo varchar(100)  NOT NULL,
+    descripcion varchar(160)  NOT NULL,
+    fuente varchar(60)  NOT NULL,
+    fecha date  NOT NULL,
+    tipo char(1)  NOT NULL,
+    CONSTRAINT OBJETO_pk PRIMARY KEY (id_objeto,id_coleccion)
+);
+
+-- Table: RESPOSITORIO
+CREATE TABLE RESPOSITORIO (
+    id_repo serial  NOT NULL,
+    nombre varchar(60)  NOT NULL,
+    publico char(1)  NOT NULL,
+    descripcion varchar(60)  NOT NULL,
+    propietario varchar(100)  NULL,
+    CONSTRAINT RESPOSITORIO_pk PRIMARY KEY (id_repo)
+);
+
+-- Table: VIDEO
+CREATE TABLE VIDEO (
+    id_coleccion int  NOT NULL,
+    id_objeto int  NOT NULL,
+    resolucion varchar(60)  NOT NULL,
+    frames_x_segundo int  NOT NULL,
+    CONSTRAINT VIDEO_pk PRIMARY KEY (id_coleccion,id_objeto)
+);
+
+-- foreign keys
+-- Reference: AUDIO_OBJETO (table: AUDIO)
+ALTER TABLE AUDIO ADD CONSTRAINT AUDIO_OBJETO
+    FOREIGN KEY (id_objeto, id_coleccion)
+    REFERENCES OBJETO (id_objeto, id_coleccion)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: DOCUMENTO_OBJETO (table: DOCUMENTO)
+ALTER TABLE DOCUMENTO ADD CONSTRAINT DOCUMENTO_OBJETO
+    FOREIGN KEY (id_objeto, id_coleccion)
+    REFERENCES OBJETO (id_objeto, id_coleccion)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: OBJETO_COLECCION (table: OBJETO)
+ALTER TABLE OBJETO ADD CONSTRAINT OBJETO_COLECCION
+    FOREIGN KEY (id_coleccion)
+    REFERENCES COLECCION (id_coleccion)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: OBJETO_RESPOSITORIO (table: OBJETO)
+ALTER TABLE OBJETO ADD CONSTRAINT OBJETO_RESPOSITORIO
+    FOREIGN KEY (id_repo)
+    REFERENCES RESPOSITORIO (id_repo)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: VIDEO_OBJETO (table: VIDEO)
+ALTER TABLE VIDEO ADD CONSTRAINT VIDEO_OBJETO
+    FOREIGN KEY (id_objeto, id_coleccion)
+    REFERENCES OBJETO (id_objeto, id_coleccion)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- End of file.
+
