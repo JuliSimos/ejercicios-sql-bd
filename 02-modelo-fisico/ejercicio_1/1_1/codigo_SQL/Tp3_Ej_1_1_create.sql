0627db1c343223fae1b41b14b49f054bc910c598
Julieta Simos <91434678+JuliSimos@users.noreply.github.com>
2026-03-05 19:08:50 -0300

    Update Tp3_Ej_1_1_create.sql

--- 02-modelo-fisico/ejercicio_1/1_1/codigo_SQL/Tp3_Ej_1_1_create.sql
+++ 02-modelo-fisico/ejercicio_1/1_1/codigo_SQL/Tp3_Ej_1_1_create.sql
@@ -1,103 +1,103 @@
 -- Created by Redgate Data Modeler (https://datamodeler.redgate-platform.com)
--- Last modification date: 2025-12-12 04:36:22.566
+-- Last modification date: 2026-03-05 22:01:31.649
 
 -- tables
--- Table: AUDIO
-CREATE TABLE AUDIO (
+-- Table: Audio
+CREATE TABLE Audio (
     id_coleccion int  NOT NULL,
     id_objeto int  NOT NULL,
-    formato varchar(60)  NOT NULL,
+    formato varchar(30)  NOT NULL,
     duracion int  NOT NULL,
-    CONSTRAINT AUDIO_pk PRIMARY KEY (id_coleccion,id_objeto)
+    CONSTRAINT Audio_pk PRIMARY KEY (id_objeto,id_coleccion)
 );
 
--- Table: COLECCION
-CREATE TABLE COLECCION (
+-- Table: Coleccion
+CREATE TABLE Coleccion (
     id_coleccion serial  NOT NULL,
-    titulo_coleccion varchar(60)  NOT NULL,
-    descripcion varchar(160)  NOT NULL,
-    CONSTRAINT COLECCION_pk PRIMARY KEY (id_coleccion)
+    titulo_coleccion varchar(30)  NOT NULL,
+    descripcion varchar(100)  NOT NULL,
+    CONSTRAINT Coleccion_pk PRIMARY KEY (id_coleccion)
 );
 
--- Table: DOCUMENTO
-CREATE TABLE DOCUMENTO (
+-- Table: Documento
+CREATE TABLE Documento (
     id_coleccion int  NOT NULL,
     id_objeto int  NOT NULL,
     tipo_publicacion varchar(30)  NOT NULL,
-    modos_color int  NOT NULL,
-    resolucion_captura varchar(60)  NOT NULL,
-    CONSTRAINT DOCUMENTO_pk PRIMARY KEY (id_coleccion,id_objeto)
+    modos_color varchar(30)  NOT NULL,
+    resolucion_captura int  NOT NULL,
+    CONSTRAINT Documento_pk PRIMARY KEY (id_coleccion,id_objeto)
 );
 
--- Table: OBJETO
-CREATE TABLE OBJETO (
+-- Table: Objeto
+CREATE TABLE Objeto (
     id_coleccion int  NOT NULL,
     id_objeto serial  NOT NULL,
-    id_repo int  NOT NULL,
-    titulo varchar(100)  NOT NULL,
+    titulo varchar(60)  NOT NULL,
     descripcion varchar(160)  NOT NULL,
-    fuente varchar(60)  NOT NULL,
+    fuente varchar(50)  NOT NULL,
     fecha date  NOT NULL,
     tipo char(1)  NOT NULL,
-    CONSTRAINT OBJETO_pk PRIMARY KEY (id_objeto,id_coleccion)
+    id_repo int  NOT NULL,
+    CONSTRAINT Objeto_pk PRIMARY KEY (id_objeto,id_coleccion)
 );
 
--- Table: RESPOSITORIO
-CREATE TABLE RESPOSITORIO (
+-- Table: Repositorio
+CREATE TABLE Repositorio (
     id_repo serial  NOT NULL,
-    nombre varchar(60)  NOT NULL,
-    publico char(1)  NOT NULL,
-    descripcion varchar(60)  NOT NULL,
-    propietario varchar(100)  NULL,
-    CONSTRAINT RESPOSITORIO_pk PRIMARY KEY (id_repo)
+    nombre varchar(30)  NOT NULL,
+    publico boolean  NOT NULL,
+    descripcion varchar(100)  NOT NULL,
+    duenio varchar(30)  NULL,
+    CONSTRAINT Repositorio_pk PRIMARY KEY (id_repo)
 );
 
--- Table: VIDEO
-CREATE TABLE VIDEO (
+-- Table: Video
+CREATE TABLE Video (
     id_coleccion int  NOT NULL,
     id_objeto int  NOT NULL,
-    resolucion varchar(60)  NOT NULL,
+    resolucion varchar(30)  NOT NULL,
     frames_x_segundo int  NOT NULL,
-    CONSTRAINT VIDEO_pk PRIMARY KEY (id_coleccion,id_objeto)
+    CONSTRAINT Video_pk PRIMARY KEY (id_coleccion,id_objeto)
 );
 
 -- foreign keys
--- Reference: AUDIO_OBJETO (table: AUDIO)
-ALTER TABLE AUDIO ADD CONSTRAINT AUDIO_OBJETO
-    FOREIGN KEY (id_objeto, id_coleccion)
-    REFERENCES OBJETO (id_objeto, id_coleccion)  
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE
-;
-
--- Reference: DOCUMENTO_OBJETO (table: DOCUMENTO)
-ALTER TABLE DOCUMENTO ADD CONSTRAINT DOCUMENTO_OBJETO
-    FOREIGN KEY (id_objeto, id_coleccion)
-    REFERENCES OBJETO (id_objeto, id_coleccion)  
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE
-;
-
--- Reference: OBJETO_COLECCION (table: OBJETO)
-ALTER TABLE OBJETO ADD CONSTRAINT OBJETO_COLECCION
+-- Reference: FK_Coleccion_Objeto (table: Objeto)
+ALTER TABLE Objeto ADD CONSTRAINT FK_Coleccion_Objeto
     FOREIGN KEY (id_coleccion)
-    REFERENCES COLECCION (id_coleccion)  
+    REFERENCES Coleccion (id_coleccion)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
--- Reference: OBJETO_RESPOSITORIO (table: OBJETO)
-ALTER TABLE OBJETO ADD CONSTRAINT OBJETO_RESPOSITORIO
-    FOREIGN KEY (id_repo)
-    REFERENCES RESPOSITORIO (id_repo)  
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE
-;
-
--- Reference: VIDEO_OBJETO (table: VIDEO)
-ALTER TABLE VIDEO ADD CONSTRAINT VIDEO_OBJETO
+-- Reference: FK_Objeto_Audio (table: Audio)
+ALTER TABLE Audio ADD CONSTRAINT FK_Objeto_Audio
     FOREIGN KEY (id_objeto, id_coleccion)
-    REFERENCES OBJETO (id_objeto, id_coleccion)  
+    REFERENCES Objeto (id_objeto, id_coleccion)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: FK_Objeto_Documento (table: Documento)
+ALTER TABLE Documento ADD CONSTRAINT FK_Objeto_Documento
+    FOREIGN KEY (id_objeto, id_coleccion)
+    REFERENCES Objeto (id_objeto, id_coleccion)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: FK_Objeto_Video (table: Video)
+ALTER TABLE Video ADD CONSTRAINT FK_Objeto_Video
+    FOREIGN KEY (id_objeto, id_coleccion)
+    REFERENCES Objeto (id_objeto, id_coleccion)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: FK_repositorio_objeto (table: Objeto)
+ALTER TABLE Objeto ADD CONSTRAINT FK_repositorio_objeto
+    FOREIGN KEY (id_repo)
+    REFERENCES Repositorio (id_repo)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
