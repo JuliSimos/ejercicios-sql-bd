0808f191bb6b3cb3cac9d1ce5bc8c24d662c6bab
Julieta Simos <91434678+JuliSimos@users.noreply.github.com>
2026-03-06 00:33:21 -0300

    Update Tp3_Ej_1_2_create.sql

--- 02-modelo-fisico/ejercicio_1/1_2/codigo_SQL/Tp3_Ej_1_2_create.sql
+++ 02-modelo-fisico/ejercicio_1/1_2/codigo_SQL/Tp3_Ej_1_2_create.sql
@@ -1,66 +1,64 @@
 -- Created by Redgate Data Modeler (https://datamodeler.redgate-platform.com)
--- Last modification date: 2025-12-12 17:06:53.827
+-- Last modification date: 2026-03-06 03:23:17.381
 
 -- tables
--- Table: AUTOR
-CREATE TABLE AUTOR (
+-- Table: Autor
+CREATE TABLE Autor (
     cod_catalogo int  NOT NULL,
-    autor varchar(100)  NOT NULL,
-    CONSTRAINT pk_autor PRIMARY KEY (cod_catalogo)
+    nombre_autor varchar(30)  NOT NULL,
+    CONSTRAINT Autor_pk PRIMARY KEY (cod_catalogo)
 );
 
--- Table: CATALOGO_LIBRO
-CREATE TABLE CATALOGO_LIBRO (
-    cod_catalogo int  NOT NULL,
+-- Table: Catalogo_libro
+CREATE TABLE Catalogo_libro (
+    cod_catalogo serial  NOT NULL,
     titulo varchar(60)  NOT NULL,
-    formato varchar(10)  NOT NULL,
+    formato varchar(30)  NOT NULL,
     editorial varchar(60)  NOT NULL,
-    CONSTRAINT pk_catalogo PRIMARY KEY (cod_catalogo)
+    CONSTRAINT Catalogo_libro_pk PRIMARY KEY (cod_catalogo)
 );
 
--- Table: CON_CARNET
-CREATE TABLE CON_CARNET (
+-- Table: Con_carnet
+CREATE TABLE Con_carnet (
     id_usuario int  NOT NULL,
     nro_carnet int  NOT NULL,
-    CONSTRAINT ak_usuario_con_carnet UNIQUE (nro_carnet) NOT DEFERRABLE  INITIALLY IMMEDIATE,
-    CONSTRAINT pk_usario_con_carnet PRIMARY KEY (id_usuario)
+    CONSTRAINT Con_carnet_pk PRIMARY KEY (id_usuario)
 );
 
--- Table: EJEMPLAR_LIBRO
-CREATE TABLE EJEMPLAR_LIBRO (
+-- Table: Ejemplar_libro
+CREATE TABLE Ejemplar_libro (
     cod_catalogo int  NOT NULL,
-    nro_ejemplar int  NOT NULL,
+    nro_ejemplar serial  NOT NULL,
     anio_edicion int  NOT NULL,
     nro_edicion int  NOT NULL,
-    CONSTRAINT pk_ejemplar_libro PRIMARY KEY (nro_ejemplar,cod_catalogo)
+    CONSTRAINT Ejemplar_libro_pk PRIMARY KEY (nro_ejemplar,cod_catalogo)
 );
 
--- Table: PRESTAMO
-CREATE TABLE PRESTAMO (
-    id_prestamo serial  NOT NULL,
+-- Table: Prestamo
+CREATE TABLE Prestamo (
+    id_prestamo int  NOT NULL,
+    fecha_desde int  NOT NULL,
+    fecha_hasta int  NOT NULL,
     id_usuario int  NOT NULL,
-    fecha_desde date  NOT NULL,
-    fecha_hasta date  NOT NULL,
-    CONSTRAINT pk_prestamo PRIMARY KEY (id_prestamo)
+    CONSTRAINT Prestamo_pk PRIMARY KEY (id_prestamo)
 );
 
--- Table: SIN_CARNET
-CREATE TABLE SIN_CARNET (
+-- Table: Sin_carnet
+CREATE TABLE Sin_carnet (
     id_usuario int  NOT NULL,
     nro_celular varchar(100)  NOT NULL,
-    CONSTRAINT pk_usuario_sin_carnet PRIMARY KEY (id_usuario)
+    CONSTRAINT Sin_carnet_pk PRIMARY KEY (id_usuario)
 );
 
--- Table: USUARIO
-CREATE TABLE USUARIO (
+-- Table: Usuario
+CREATE TABLE Usuario (
     id_usuario serial  NOT NULL,
-    tipo_documento int  NOT NULL,
-    nro_documento int  NOT NULL,
-    apellido varchar(60)  NOT NULL,
-    nombre varchar(60)  NOT NULL,
+    tipo_doc varchar(30)  NOT NULL,
+    nro_doc int  NOT NULL,
+    apellido varchar(30)  NOT NULL,
+    nombre varchar(30)  NOT NULL,
     e_mail varchar(60)  NOT NULL,
-    tipo char(1)  NOT NULL,
-    CONSTRAINT ak_usuario UNIQUE (tipo_documento, nro_documento) NOT DEFERRABLE  INITIALLY IMMEDIATE,
+    CONSTRAINT ak_usuario UNIQUE (tipo_doc, nro_doc) NOT DEFERRABLE  INITIALLY IMMEDIATE,
     CONSTRAINT pk_usuario PRIMARY KEY (id_usuario)
 );
 
@@ -69,62 +67,62 @@ CREATE TABLE prestamo_libro (
     id_prestamo int  NOT NULL,
     nro_ejemplar int  NOT NULL,
     cod_catalogo int  NOT NULL,
-    CONSTRAINT pk_prestamo_libro PRIMARY KEY (id_prestamo,nro_ejemplar,cod_catalogo)
+    CONSTRAINT prestamo_libro_pk PRIMARY KEY (id_prestamo)
 );
 
 -- foreign keys
--- Reference: fk_autores_catalogo_libro (table: AUTOR)
-ALTER TABLE AUTOR ADD CONSTRAINT fk_autores_catalogo_libro
+-- Reference: Autor_Catalogo_libro (table: Autor)
+ALTER TABLE Autor ADD CONSTRAINT Autor_Catalogo_libro
     FOREIGN KEY (cod_catalogo)
-    REFERENCES CATALOGO_LIBRO (cod_catalogo)  
+    REFERENCES Catalogo_libro (cod_catalogo)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
--- Reference: fk_con_carnet_usuario (table: CON_CARNET)
-ALTER TABLE CON_CARNET ADD CONSTRAINT fk_con_carnet_usuario
-    FOREIGN KEY (id_usuario)
-    REFERENCES USUARIO (id_usuario)  
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE
-;
-
--- Reference: fk_ejemplar_libro_catalogo_libro (table: EJEMPLAR_LIBRO)
-ALTER TABLE EJEMPLAR_LIBRO ADD CONSTRAINT fk_ejemplar_libro_catalogo_libro
+-- Reference: fk_Catalogo_libro_Ejemplar_libro (table: Ejemplar_libro)
+ALTER TABLE Ejemplar_libro ADD CONSTRAINT fk_Catalogo_libro_Ejemplar_libro
     FOREIGN KEY (cod_catalogo)
-    REFERENCES CATALOGO_LIBRO (cod_catalogo)  
+    REFERENCES Catalogo_libro (cod_catalogo)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
--- Reference: fk_prestamo_con_carnet (table: PRESTAMO)
-ALTER TABLE PRESTAMO ADD CONSTRAINT fk_prestamo_con_carnet
+-- Reference: fk_Con_carnet_Prestamo (table: Prestamo)
+ALTER TABLE Prestamo ADD CONSTRAINT fk_Con_carnet_Prestamo
     FOREIGN KEY (id_usuario)
-    REFERENCES CON_CARNET (nro_carnet)  
+    REFERENCES Con_carnet (id_usuario)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
--- Reference: fk_prestamo_libro_con_carnet (table: prestamo_libro)
-ALTER TABLE prestamo_libro ADD CONSTRAINT fk_prestamo_libro_con_carnet
+-- Reference: fk_Prestamo_prestamo_libro (table: prestamo_libro)
+ALTER TABLE prestamo_libro ADD CONSTRAINT fk_Prestamo_prestamo_libro
     FOREIGN KEY (id_prestamo)
-    REFERENCES PRESTAMO (id_prestamo)  
+    REFERENCES Prestamo (id_prestamo)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
--- Reference: fk_prestamo_libro_ejemplar_libro (table: prestamo_libro)
-ALTER TABLE prestamo_libro ADD CONSTRAINT fk_prestamo_libro_ejemplar_libro
-    FOREIGN KEY (nro_ejemplar, cod_catalogo)
-    REFERENCES EJEMPLAR_LIBRO (nro_ejemplar, cod_catalogo)  
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE
-;
-
--- Reference: fk_sin_carnet_usuario (table: SIN_CARNET)
-ALTER TABLE SIN_CARNET ADD CONSTRAINT fk_sin_carnet_usuario
+-- Reference: fk_Usuario_Con_carnet (table: Con_carnet)
+ALTER TABLE Con_carnet ADD CONSTRAINT fk_Usuario_Con_carnet
     FOREIGN KEY (id_usuario)
-    REFERENCES USUARIO (id_usuario)  
+    REFERENCES Usuario (id_usuario)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: fk_Usuario_Sin_carnet (table: Sin_carnet)
+ALTER TABLE Sin_carnet ADD CONSTRAINT fk_Usuario_Sin_carnet
+    FOREIGN KEY (id_usuario)
+    REFERENCES Usuario (id_usuario)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: fk_ejemplar_libro_prestamo_libro (table: prestamo_libro)
+ALTER TABLE prestamo_libro ADD CONSTRAINT fk_ejemplar_libro_prestamo_libro
+    FOREIGN KEY (nro_ejemplar, cod_catalogo)
+    REFERENCES Ejemplar_libro (nro_ejemplar, cod_catalogo)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
