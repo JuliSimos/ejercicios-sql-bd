7052f4e84f123eaee06adbd132835d0118cd272b
Julieta Simos <91434678+JuliSimos@users.noreply.github.com>
2026-03-09 17:09:57 -0300

    Update TP3_ej2_3_create.sql

--- 02-modelo-fisico/ejercicio_2/2_3/codigo_SQL/TP3_ej2_3_create.sql
+++ 02-modelo-fisico/ejercicio_2/2_3/codigo_SQL/TP3_ej2_3_create.sql
@@ -1,85 +1,86 @@
 -- Created by Redgate Data Modeler (https://datamodeler.redgate-platform.com)
--- Last modification date: 2025-12-12 20:06:06.778
+-- Last modification date: 2026-03-09 20:09:20.185
 
 -- tables
--- Table: CAMION
-CREATE TABLE CAMION (
+-- Table: Camion
+CREATE TABLE Camion (
     matricula int  NOT NULL,
     modelo varchar(30)  NOT NULL,
-    tipo int  NOT NULL,
+    tipo varchar(30)  NOT NULL,
     potencia int  NOT NULL,
-    CONSTRAINT CAMION_pk PRIMARY KEY (matricula)
+    CONSTRAINT pk_Camion PRIMARY KEY (matricula)
 );
 
--- Table: CAMIONERO
-CREATE TABLE CAMIONERO (
-    DNI int  NOT NULL,
+-- Table: Camionero
+CREATE TABLE Camionero (
+    dni int  NOT NULL,
     nombre varchar(30)  NOT NULL,
+    apellido varchar(30)  NOT NULL,
     telefono varchar(30)  NOT NULL,
+    salario decimal  NOT NULL,
+    ciudad varchar(60)  NOT NULL,
     direccion_calle varchar(60)  NOT NULL,
     direccion_puerta int  NOT NULL,
     direccion_piso int  NULL,
-    salario decimal  NOT NULL,
-    ciudad varchar(60)  NOT NULL,
-    CONSTRAINT CAMIONERO_pk PRIMARY KEY (DNI)
+    CONSTRAINT pk_Camionero PRIMARY KEY (dni)
 );
 
--- Table: CIUDAD
-CREATE TABLE CIUDAD (
-    cod_ciudad serial  NOT NULL,
-    nombre_ciudad varchar(60)  NOT NULL,
-    CONSTRAINT CIUDAD_pk PRIMARY KEY (cod_ciudad)
+-- Table: Ciudad
+CREATE TABLE Ciudad (
+    codig_ciudad serial  NOT NULL,
+    nombre varchar(100)  NOT NULL,
+    CONSTRAINT pk_Ciudad PRIMARY KEY (codig_ciudad)
 );
 
--- Table: CONDUCE
-CREATE TABLE CONDUCE (
-    dni_camionero int  NOT NULL,
+-- Table: Conduce
+CREATE TABLE Conduce (
     matricula_camion int  NOT NULL,
+    dni_camionero int  NOT NULL,
     fecha_inicio date  NOT NULL,
     fecha_fin date  NOT NULL,
-    CONSTRAINT CONDUCE_pk PRIMARY KEY (matricula_camion,dni_camionero)
+    CONSTRAINT pk_Conduce PRIMARY KEY (matricula_camion,dni_camionero)
 );
 
--- Table: PAQUETE
-CREATE TABLE PAQUETE (
-    cod_paquete serial  NOT NULL,
-    descripcion varchar(100)  NOT NULL,
+-- Table: Paquete
+CREATE TABLE Paquete (
+    codig_paquete serial  NOT NULL,
+    dni_camionero int  NOT NULL,
+    codig_ciudad int  NOT NULL,
+    descripcion varchar(200)  NOT NULL,
     destinatario varchar(60)  NOT NULL,
     direccion_destino varchar(100)  NOT NULL,
-    cod_ciudad int  NOT NULL,
-    dni_camionero int  NOT NULL,
-    CONSTRAINT PAQUETE_pk PRIMARY KEY (cod_paquete)
+    CONSTRAINT pk_Paquete PRIMARY KEY (codig_paquete)
 );
 
 -- foreign keys
--- Reference: FK_CAMIONERO_CONDUCE (table: CONDUCE)
-ALTER TABLE CONDUCE ADD CONSTRAINT FK_CAMIONERO_CONDUCE
-    FOREIGN KEY (dni_camionero)
-    REFERENCES CAMIONERO (DNI)  
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE
-;
-
--- Reference: FK_CAMIONERO_PAQUETE (table: PAQUETE)
-ALTER TABLE PAQUETE ADD CONSTRAINT FK_CAMIONERO_PAQUETE
-    FOREIGN KEY (dni_camionero)
-    REFERENCES CAMIONERO (DNI)  
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE
-;
-
--- Reference: FK_CIUDAD_PAQUETE (table: PAQUETE)
-ALTER TABLE PAQUETE ADD CONSTRAINT FK_CIUDAD_PAQUETE
-    FOREIGN KEY (cod_ciudad)
-    REFERENCES CIUDAD (cod_ciudad)  
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE
-;
-
--- Reference: FK_CONDUCE_CAMION (table: CONDUCE)
-ALTER TABLE CONDUCE ADD CONSTRAINT FK_CONDUCE_CAMION
+-- Reference: fk_Camion_Conduce (table: Conduce)
+ALTER TABLE Conduce ADD CONSTRAINT fk_Camion_Conduce
     FOREIGN KEY (matricula_camion)
-    REFERENCES CAMION (matricula)  
+    REFERENCES Camion (matricula)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: fk_Camionero_Conduce (table: Conduce)
+ALTER TABLE Conduce ADD CONSTRAINT fk_Camionero_Conduce
+    FOREIGN KEY (dni_camionero)
+    REFERENCES Camionero (dni)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: fk_Camionero_Paquete (table: Paquete)
+ALTER TABLE Paquete ADD CONSTRAINT fk_Camionero_Paquete
+    FOREIGN KEY (dni_camionero)
+    REFERENCES Camionero (dni)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: fk_Ciudad_Paquete (table: Paquete)
+ALTER TABLE Paquete ADD CONSTRAINT fk_Ciudad_Paquete
+    FOREIGN KEY (codig_ciudad)
+    REFERENCES Ciudad (codig_ciudad)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
