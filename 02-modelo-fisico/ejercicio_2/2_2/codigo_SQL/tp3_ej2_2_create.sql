4e63ed29e82ac72d92939738021ed3e6e84b7a28
Julieta Simos <91434678+JuliSimos@users.noreply.github.com>
2026-03-09 18:39:34 -0300

    Update tp3_ej2_2_create.sql

--- 02-modelo-fisico/ejercicio_2/2_2/codigo_SQL/tp3_ej2_2_create.sql
+++ 02-modelo-fisico/ejercicio_2/2_2/codigo_SQL/tp3_ej2_2_create.sql
@@ -1,27 +1,28 @@
 -- Created by Redgate Data Modeler (https://datamodeler.redgate-platform.com)
--- Last modification date: 2026-03-09 19:29:36.824
+-- Last modification date: 2026-03-09 21:38:06.213
 
 -- tables
 -- Table: Departamento
 CREATE TABLE Departamento (
     cod_depto serial  NOT NULL,
-    nombre_depto int  NOT NULL,
+    nombre_depto varchar(100)  NOT NULL,
     id_jefe int  NOT NULL,
-    CONSTRAINT ak_Departamento UNIQUE (nombre_depto) NOT DEFERRABLE  INITIALLY IMMEDIATE,
+    CONSTRAINT ak_nombre_depto UNIQUE (nombre_depto) NOT DEFERRABLE  INITIALLY IMMEDIATE,
+    CONSTRAINT ak_jefe_depto UNIQUE (id_jefe) NOT DEFERRABLE  INITIALLY IMMEDIATE,
     CONSTRAINT pk_Departamento PRIMARY KEY (cod_depto)
 );
 
 -- Table: Empleado
 CREATE TABLE Empleado (
     nro_empleado serial  NOT NULL,
+    cod_depto int  NOT NULL,
     nombre varchar(60)  NOT NULL,
     apellido varchar(60)  NOT NULL,
-    direccion_calle int  NOT NULL,
-    direccion_puerta int  NOT NULL,
+    direccion_calle varchar(100)  NOT NULL,
+    direccion_puerta varchar(30)  NOT NULL,
     direccion_piso int  NULL,
-    direccion_ciudad int  NOT NULL,
+    direccion_ciudad varchar(60)  NOT NULL,
     tipo char(1)  NOT NULL,
-    cod_depto int  NOT NULL,
     CONSTRAINT ak_Empleado UNIQUE (cod_depto) NOT DEFERRABLE  INITIALLY IMMEDIATE,
     CONSTRAINT pk_Empleado PRIMARY KEY (nro_empleado)
 );
